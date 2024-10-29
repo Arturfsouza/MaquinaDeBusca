8247198c9fd4e5f8dc0f468cd8ed9fb296240194
Artur <artur.fonseca.souza@gmail.com>
2024-10-29 00:58:46 -0300

    resolvido sintaxe errada da definicao de metodos

--- indice.h
+++ indice.h
@@ -1,37 +1,27 @@
+#ifndef INDICE_H
+#define INDICE_H
+
+
 #include <iostream>
 #include <map>
-#include <cctype>
 #include <string>
 #include <vector>
-#include <fstream>
-#include <sstream>
+
 
 
 class Indice {
     public:
         Indice(){}
-
-        void LerArquivos(const std::vector<std::string>& arquivos){
-
-        }
-
-        void AdicionarPalavra(std::string palavra, std::string nomeArquivo){
-    
-        }   
-
-        ~Indice(){
-
-        }
+        void LerArquivos(const std::vector<std::string>& arquivos);
+        void AdicionarPalavra(std::string palavra, std::string nomeArquivo);
+        ~Indice(){}
 
     private:
 
-        void ProcessarLinha(const std::string& linha, const std::string& nomeArquivo){
-
-        }
-
+        void ProcessarLinha(const std::string& linha, const std::string& nomeArquivo);
         std::map<std::string,std::map <std::string, int>> indice_;
         std::string NormalizarPalavra(std::string palavra){
-
         }
-
 };
+
+#endif
