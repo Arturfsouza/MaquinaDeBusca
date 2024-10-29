c59b10ae5508cab29047cdb83257676cd6314755
Artur <artur.fonseca.souza@gmail.com>
2024-10-29 00:59:52 -0300

    leitura de arquivos implementada em main

--- main.cpp
+++ main.cpp
@@ -2,5 +2,11 @@
 #include "indice.h"
 
 int main(){
-    
+    Indice indice;
+    std::vector<std::string> arquivos;
+    for(int i=1; i<4;i++){
+        std::string arquivo = "d" + std::to_string(i) + ".txt";
+        arquivos.push_back(arquivo);
+    }
+    indice.LerArquivos(arquivos);
 }
